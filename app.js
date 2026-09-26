fb362e827a4cd8fba48e6992de1a25446298a2c1
Abhinav Tomar <abhinavtomar856@gmail.com>
2026-09-26 21:21:14 +0530

    New Changes

--- app.js
+++ app.js
@@ -3,40 +3,40 @@ const app = express();
 const ExpressError = require("./ExpressError")
 
 //Utility Midddleware
-// app.use((req,res,next) =>{
-//     req.time = new Date(Date.now()).toString();
-//     console.log(req.path,req.hostname,req.method,req.time);
-//     return next();
-// })
+app.use((req,res,next) =>{
+    req.time = new Date(Date.now()).toString();
+    console.log(req.path,req.hostname,req.method,req.time);
+    return next();
+})
 
-// // //Multiple middlewares
-// const checkToken = (req, res, next) => {
-//     let { token } = req.query;
-//     if (token === "giveaccess") {
-//         next();
-//     }
-//     throw new ExpressError(401,"ACCESS DENIED!");
-// };
+// //Multiple middlewares
+const checkToken = (req, res, next) => {
+    let { token } = req.query;
+    if (token === "giveaccess") {
+        next();
+    }
+    throw new ExpressError(401,"ACCESS DENIED!");
+};
 
-// app.get("/api", checkToken, (req, res) => {
-//     res.send("data");
-// });
+app.get("/api", checkToken, (req, res) => {
+    res.send("data");
+});
 
-// app.use((err,req,res,next) =>{
-//     let{status,message} = err;
-//     console.log("ERROR OCCURED");
-//     res.status(status).send(message);
-// })
+app.use((err,req,res,next) =>{
+    let{status,message} = err;
+    console.log("ERROR OCCURED");
+    res.status(status).send(message);
+})
 
-// //Error Handler(Middleware)
-// app.get("/err",(req,res)=>{
-//     abcd = abcd;
-// });
+//Error Handler(Middleware)
+app.get("/err",(req,res)=>{
+    abcd = abcd;
+});
 
-// app.use((err,req,res,next) =>{
-//     let{status=500,message="Some error occured"} = err;
-//     res.status(status).send(message);
-// });
+app.use((err,req,res,next) =>{
+    let{status=500,message="Some error occured"} = err;
+    res.status(status).send(message);
+});
 
 function asyncWrap(fn){
     return function(req,res,next){
@@ -54,10 +54,10 @@ app.use((err,req,res,next)=>{
     res.status(status).send(message);
 });
 
-// app.use((err,req,res,next) =>{
-//     console.log(err);
-//     next()
-// });
+app.use((err,req,res,next) =>{
+    console.log(err);
+    next()
+});
 
 app.use("/",(req,res)=>{
     res.send("Hi i am root");
